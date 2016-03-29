cef7b29cbcf7fab03620c104daf0c114ce1d6c0a
xq5he <shexinquan@gmail.com>
2016-03-29 17:05:21 +0800

    Add House Robber II

--- house_robber.py
+++ house_robber.py
@@ -9,3 +9,17 @@ def rob(nums):
         max_money = p2 if p2 > p1 + num else p1 + num
         p1, p2 = p2, max_money
     return max_money
+
+
+def rob2(nums):
+    """
+    problem: House Robber II
+    source: https://leetcode.com/problems/house-robber-ii/
+
+    """
+    size = len(nums)
+    if size == 1:
+        return nums[0]
+    m1 = rob(nums[1:])
+    m2 = rob(nums[:-1])
+    return m1 if m1 > m2 else m2
