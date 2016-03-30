55a9dfd764f3a402999b9d0f9ff60284f148f4d9
xq5he <shexinquan@gmail.com>
2016-03-30 16:17:29 +0800

    Add House Robber III

--- house_robber.py
+++ house_robber.py
@@ -23,3 +23,37 @@ def rob2(nums):
     m1 = rob(nums[1:])
     m2 = rob(nums[:-1])
     return m1 if m1 > m2 else m2
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+def rob3(root):
+    """
+    problems: House Robber III
+    source: https://leetcode.com/problems/house-robber-iii/
+    """
+    # global dict used to keep max money of a node which has been calculated
+    memo = {}
+
+    def _rob(root):
+        if root is None:
+            return 0
+        # rob root
+        left_gc = [root.left.left, root.left.right] if root.left else []
+        right_gc = [root.right.left, root.right.right] if root.right else []
+        grandchildren = left_gc + right_gc
+        money_root = root.val
+        for gc in grandchildren:
+            memo[gc] = memo.get(gc) or _rob(gc)
+            money_root += memo[gc]
+        # do not rob root
+        money_left = memo.get(root.left) or _rob(root.left)
+        money_right = memo.get(root.right) or _rob(root.right)
+        memo[root.left], memo[root.right] = money_left, money_right
+
+        return max(money_root, money_left + money_right)
+    return _rob(root)
